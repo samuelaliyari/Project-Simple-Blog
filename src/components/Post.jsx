c545544240178111654e2c958ae9480e2bcb9e60
Samuel Aliyari <samuelaliyari@outlook.de>
2023-11-30 09:11:06 +0100

    V1.0.1

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,22 +2,13 @@ import { Link } from "react-router-dom";
 import "./Post.css"
 const Post = (props) => {
 
-    const postObject = {
-        "title" : props.title,
-        "id" : props.id,
-        "image" : props.image,
-        "date" : props.date,
-        "description" : props.description,
-        "author" : props.author
-    }
-
 	return (
 		<section className='post'>
 			<img
-				src={postObject.image}
+				src={props.image}
 				alt=''
 			/>
-			<h3>{postObject.title}</h3>
+			<h3>{props.title}</h3>
 			<Link
 				to='/posts'
 				state={{
